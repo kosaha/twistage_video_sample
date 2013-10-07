1aa69cb8139774841181d4a86dd243c17ee72144
kosaha <kosaha@lexmark.com>
2013-10-07 12:10:29 +0530

    Added main page controller to show videos

--- app/controllers/twistage_controller.rb
+++ app/controllers/twistage_controller.rb
@@ -1,4 +1,14 @@
+require 'helix'
+
 class TwistageController < ApplicationController
   def mainpage
+    Helix::Config.load('./config/helix.yml')
+    @videos = Helix::Video.find_all
+    puts @videos.to_json
+  end
+  
+  def show
+    @video_id = params[:id]
+    @video_title = params[:title]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 TwistageVideoSample::Application.routes.draw do
   get "twistage/mainpage"
+  
+  get "twistage/show"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
